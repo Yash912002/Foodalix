11d144bb46dcecbd43f009c0c8b0351d7b929b04
Yash912002 <92615501+Yash912002@users.noreply.github.com>
2024-01-21 19:09:20 +0530

    Fixed the CORS issue of Menu API

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -5,4 +5,5 @@ export const LOGO_URL =
 	"https://images-platform.99static.com//Ba6VdSQsbU4OpiyQEzLi7yHy9KQ=/440x521:1494x1575/fit-in/500x500/99designs-contests-attachments/127/127439/attachment_127439993";
 
 export const MENU_API =
-	"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5912716&lng=73.73890899999999&restaurantId=";
+	"https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D18.5912716%26lng%3D73.73890899999999%26restaurantId%3D";
+// "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5912716&lng=73.73890899999999&restaurantId=";
